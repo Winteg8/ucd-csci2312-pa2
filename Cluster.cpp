054f3be33c0139fdb6f7f9a6312108602f075c1f
Winteg8 <Winteg8@gmail.com>
2016-05-08 23:01:52 -0700

    Added files via upload
    Passes 144/204 tests with no runtime errors

--- Cluster.cpp
+++ Cluster.cpp
@@ -11,238 +11,302 @@
 #include "Point.h"
 #include "Cluster.h"
 
-using namespace Clustering;
-
-LNode::LNode(const Point &p, LNodePtr n)
+namespace Clustering
 {
-    point = p;
-    next = n;
-}
-
-Cluster::Cluster()
-{
-    __size = 0;
-    Point p;
-    LNodePtr ptr;
-    __points = new LNode(p, ptr);
-}
-
-Cluster & Cluster::operator=(const Cluster & equate)
-{
-    LNodePtr ptr1 = __points, ptr2 = equate.__points;
-    do
+    // NODE STUFF
+    LNode::LNode(const Point &p, LNodePtr n) : point(3)
     {
-        ptr1->point = ptr2->point;
-        
-        ptr1 = ptr1->next;
-        ptr2 = ptr2->next;
-    }
-    while(ptr1->next != nullptr);
-        
-    return *this;
-}
-
-Cluster::~Cluster()
-{
-    LNodePtr cursor = __points, previous = nullptr;
-    while (cursor->next != nullptr)
-    {
-        delete previous->next;
-        previous = cursor;
-        cursor = cursor->next;
-    }
-    delete cursor;
-    delete []__points;
-    delete this;
-}
-
-int Cluster::getSize() const
-{
-    return __size;
-}
-
-void Cluster::add(const Point & p)
-{
-    LNodePtr cursor = __points;
-    while (cursor->next != nullptr)
-    {
-        cursor = cursor->next;
+        point = p;
+        next = n;
     }
     
-    cursor->next = new LNode(p, nullptr);
-    __size++;
-}
-
-
-const Point & Cluster::remove(const Point & p)
-{
-    LNodePtr cursor = __points, previous = nullptr;
-    while (cursor->next != nullptr)
+    
+    
+    
+    // CLUSTER STUFF
+    Cluster::Cluster()
     {
-        if (p == cursor->point)
+        __size = 0;
+        Point p(3);
+        __points = nullptr;
+    }
+    
+    Cluster::Cluster(const Cluster & copyThis)
+    {
+        __size = copyThis.__size;
+        LNodePtr copyCursor = copyThis.__points;
+        
+        
+        if (copyCursor != nullptr)
         {
-            previous->next = cursor->next;
-            __size--;
-            return cursor->point;
+            __points = new LNode(copyCursor->point, nullptr);
+            copyCursor = copyCursor->next;
         }
-        else
+            
+        
+        LNodePtr cursor = __points;
+        while (copyCursor != nullptr)
+        {
+            cursor->next = new LNode(copyCursor->point, nullptr);
+            
+            cursor = cursor->next;
+            copyCursor = copyCursor->next;
+        }
+        
+        if (copyThis.__points == nullptr)
+            __points = nullptr;
+        
+    }
+    
+    Cluster & Cluster::operator=(const Cluster & equate)
+    {
+        __size = equate.__size;
+        
+        LNodePtr ptr1 = __points, ptr2 = equate.__points;
+        while (ptr1 != nullptr)
+        {
+            ptr1->point = ptr2->point;
+            
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        
+        return *this;
+    }
+    
+    Cluster::~Cluster()
+    {
+        LNodePtr current = __points, following = nullptr;
+        while (current != nullptr)
+        {
+            following = current->next;
+            delete current->next;
+            current = following;
+        }
+        delete current;
+        delete __points;
+    }
+    
+    int Cluster::getSize() const
+    {
+        return __size;
+    }
+    
+    void Cluster::add(const Point & p)
+    {
+        __points = new LNode(p, __points);
+        
+        /*LNodePtr cursor = __points, previous = __points;
+        
+        while (cursor != nullptr)
+        {
+            previous = cursor;
+            
+            if (p >= cursor->point)
+            {
+                cursor->next = new LNode(p, cursor->next);
+                break;
+            }
+            else if (p < __points->point)
+            {
+                __points = new LNode(p, __points->next);
+                break;
+            }
+            else // iterate
+                cursor = cursor->next;
+                
+        }
+        
+        if (__points == nullptr)
+            __points = new LNode(p, nullptr);*/
+        
+        __size++;
+    }
+    
+    
+    const Point & Cluster::remove(const Point & p)
+    {
+        LNodePtr cursor = __points, previous = __points;
+        while (cursor != nullptr)
+        {
+            if (p == cursor->point)
+            {
+                previous->next = cursor->next;
+                __size--;
+                return cursor->point;
+            }
+            else
+            {
+                previous = cursor;
+                cursor = cursor->next;
+            }
+        }
+        
+        return p;
+    }
+    
+    
+    bool Cluster::contains(const Point & p)
+    {
+        LNodePtr cursor = __points;
+        
+        if (cursor->point == p)
+            return true;
+        
+        while (cursor->next != nullptr)
+        {
+            if (p == cursor->point)
+                return true;
+            else
+                cursor = cursor->next;
+        }
+        
+        return false;
+    }
+    
+    const Point & Cluster::operator[](unsigned int index) const
+    {
+        LNodePtr cursor = __points, previous = __points;
+        int i = 0;
+        
+        while ((cursor != nullptr) && (i < index))
         {
             previous = cursor;
             cursor = cursor->next;
+            i++;
         }
+        
+        return cursor->point;
     }
     
-    return p;
-}
-
-
-bool Cluster::contains(const Point & p)
-{
-    LNodePtr cursor = __points;
-    while (cursor->next != nullptr)
+    Cluster & Cluster::operator+=(const Point & p)
     {
-        if (p == cursor->point)
-            return true;
-        else
+        this->add(p);
+        return *this;
+    }
+    
+    Cluster & Cluster::operator-=(const Point & p)
+    {
+        if (contains(p))
+            remove(p);
+        return *this;
+    }
+    
+    Cluster & Cluster::operator+=(const Cluster & c)
+    {
+        LNodePtr cursor = c.__points;
+        while (cursor != nullptr)
+        {
+            this->add(c.__points->point);
+            __size++;
             cursor = cursor->next;
+        }
+        return *this;
     }
     
-    return false;
-}
-
-const Point & Cluster::operator[](unsigned int index) const
-{
-    LNodePtr cursor = __points;
-    int i;
-    while (cursor->next != nullptr && i < index)
+    Cluster & Cluster::operator-=(const Cluster & c)
     {
-        cursor = cursor->next;
-        i++;
+        LNodePtr cursor = c.__points;
+        while (cursor != nullptr)
+        {
+            if (contains(cursor->point))
+                {
+                    this->remove(c.__points->point);
+                    __size--;
+                }
+            
+            cursor = cursor->next;
+        }
+        return *this;
     }
     
-    return cursor->point;
-}
-
-Cluster & Cluster::operator+=(const Point & p)
-{
-    this->add(p);
-    return *this;
-}
-
-Cluster & Cluster::operator-=(const Point & p)
-{
-    this->remove(p);
-    return *this;
-}
-
-Cluster & Cluster::operator+=(const Cluster & c)
-{
-    LNodePtr cursor = c.__points;
-    while (cursor->next != nullptr)
+    
+    //Friends
+    
+    std::ostream & Clustering::operator<<(std::ostream & stream, const Cluster & c)
     {
-        this->add(c.__points->point);
-        __size++;
-        cursor = cursor->next;
-    }
-    return *this;
-}
-
-Cluster & Cluster::operator-=(const Cluster & c)
-{
-    LNodePtr cursor = c.__points;
-    while (cursor->next != nullptr)
-    {
-        this->remove(c.__points->point);
         
-        // CHECK THIS LATER CUZ THIS WON'T WORK IN ALL SCENARIOS
-        __size--;
-        cursor = cursor->next;
-    }
-    return *this;
-}
-
-
-//Friends
-
-std::ostream & Clustering::operator<<(std::ostream & stream, const Cluster & c)
-{
-    
-    LNodePtr cursor = c.__points;
-    while (cursor->next != nullptr)
-    {
-        stream << c.__points->point;
-        cursor = cursor->next;
-    }
-    
-    return stream;
-}
-
-std::istream & Clustering::operator>>(std::istream & stream, Cluster & c)
-{
-    LNodePtr cursor = c.__points;
-    while (cursor->next != nullptr)
-    {
-        stream >> c.__points->point;
-        cursor = cursor->next;
-    }
-    
-    return stream;
-}
-
-bool Clustering::operator==(const Cluster & c1, const Cluster & c2)
-{
-    LNodePtr cursor1 = c1.__points, cursor2 = c2.__points;
-    while (cursor1->next != nullptr)
-    {
-        if (cursor1->point != cursor2->point)
-            return false;
-        cursor1 = cursor1->next;
-        cursor2 = cursor2->next;
+        LNodePtr cursor = c.__points;
+        while (cursor != nullptr)
+        {
+            stream << c.__points->point;
+            cursor = cursor->next;
+        }
         
+        return stream;
     }
-    return true;
-}
-
-bool Clustering::operator!=(const Cluster & c1, const Cluster & c2)
-{
-    if (c1 == c2)
+    
+    std::istream & Clustering::operator>>(std::istream & stream, Cluster & c)
+    {
+        LNodePtr cursor = c.__points;
+        while (cursor != nullptr)
+        {
+            stream >> c.__points->point;
+            cursor = cursor->next;
+        }
+        
+        return stream;
+    }
+    
+    bool Clustering::operator==(const Cluster & c1, const Cluster & c2)
+    {
+        if (c1.__size == c2.__size)
+        {
+            LNodePtr ptr1 = c1.__points, ptr2 = c2.__points;
+            
+            while (ptr1 != nullptr && ptr2 != nullptr)
+            {
+                if (ptr1->point != ptr2->point)
+                    return false;
+                
+                ptr1 = ptr1->next;
+                ptr2 = ptr2->next;
+            }
+            
+            return true;
+        }
         return false;
-    else
-        return true;
+    }
+    
+    bool Clustering::operator!=(const Cluster & c1, const Cluster & c2)
+    {
+        if (c1 == c2)
+            return false;
+        else
+            return true;
+    }
+    
+    // Friends: Arithmetic (Cluster and Point)
+    const Cluster operator+(const Cluster & c, const Point & p)
+    {
+        Cluster answer = c;
+        answer += p;
+        return answer;
+    }
+    
+    const Cluster operator-(const Cluster & c, const Point & p)
+    {
+        Cluster answer = c;
+        answer -= p;
+        return answer;
+    }
+    
+    // Friends: Arithmetic (two Clusters)
+    const Cluster Clustering::operator+(const Cluster & c1, const Cluster & c2)
+    {
+        Cluster answer = c1;
+        answer += c2;
+        return answer;
+    }
+    
+    const Cluster Clustering::operator-(const Cluster & c1, const Cluster & c2)
+    {
+        Cluster answer = c1;
+        answer -= c2;
+        return answer;
+    }
+
+
 }
-
-// Friends: Arithmetic (Cluster and Point)
-const Cluster operator+(const Cluster & c, const Point & p)
-{
-    Cluster answer = c;
-    answer += p;
-    return answer;
-}
-
-const Cluster operator-(const Cluster & c, const Point & p)
-{
-    Cluster answer = c;
-    answer -= p;
-    return answer;
-}
-
-// Friends: Arithmetic (two Clusters)
-const Cluster Clustering::operator+(const Cluster & c1, const Cluster & c2)
-{
-    Cluster answer = c1;
-    answer += c2;
-    return answer;
-}
-
-const Cluster Clustering::operator-(const Cluster & c1, const Cluster & c2)
-{
-    Cluster answer = c1;
-    answer -= c2;
-    return answer;
-}
-
-
-
-
 
 
--- Point.cpp
+++ Point.cpp
@@ -6,266 +6,290 @@
 //  Copyright © 2016 Maxim Anaya. All rights reserved.
 //
 
+#include <iostream>
 #include <cmath>
-#include <stdio.h>
 
 #include "Point.h"
 
-using namespace Clustering;
-
-static int ID = 0;
-
-// Constructors/=operand/destructors
-
-
-Point::Point()
-{}
-
-Point::Point(int newDim)
+namespace Clustering
 {
-    __id = ID++;
+    static int ID = 0;
     
-    __dim = newDim;
-}
-
-Point::Point(int newDim, double *newVal)
-{
-    __id = ID++;
+    // Constructors/=operand/destructors
     
-    __dim = newDim;
-    __values = newVal;
-}
-
-Point::Point(const Point &copyThis)
-{
-    __id = copyThis.getId();
-    __dim = copyThis.getDims();
-    __values = new double [__dim];
-    for (int i = 0; i < __dim; i++)
-        __values[i] = copyThis.getValue(i);
-}
-
-Point & Point::operator=(const Point &equate)
-{
-    __id = equate.getId();
-    __dim = equate.getDims();
-    for (int i = 0; i < __dim; i++)
-        __values[i] = equate.getValue(i);
-    return *this;
-}
-
-Point::~Point()
-{
-    delete []__values;
-    delete this;
-}
-
-
-
-// Accessors & mutators
-
-int Point::getId() const
-{
-    return __id;
-}
-
-int Point::getDims() const
-{
-    return __dim;
-}
-
-void Point::setValue(int i, double setTo)
-{
-    __values[i] = setTo;
-}
-
-double Point::getValue(int i) const
-{
-    return __values[i];
-}
-
-
-
-// Member functions
-
-double Point::distanceTo(const Point &other) const
-{
-    if (__dim == other.getDims())
+    Point::Point(int newDim)
     {
-        double sum;
-    
+        __id = ID++;
+        
+        __dim = newDim;
+        
+        __values = new double[__dim];
         for (int i = 0; i < __dim; i++)
-            sum += pow((__values[i]-other.getValue(i)), 2);
-    
-        return sqrt(sum);
+            __values[i] = 0;
     }
-    else
+    
+    Point::Point(int newDim, double *newVal)
     {
-        std::cout << "Can't calculate between points in differnt dimensions" << std::endl;
-        return -1;
+        __id = ID++;
+        
+        __dim = newDim;
+        __values = newVal;
     }
-}
-
-
-
-// Other overloaded operators
-
-
-// Members
-
-Point & Point::operator*=(double multBy) // p *= 6; p.operator*=(6)
-{
-    for (int i = 0; i < __dim; i++)
-        __values[i] *= multBy;
     
-    return *this;
-}
-
-Point & Point::operator/=(double divideBy)
-{
-    for (int i = 0; i < __dim; i++)
-        __values[i] /= divideBy;
+    Point::Point(const Point &copyThis)
+    {
+        __id = copyThis.getId();
+        __dim = copyThis.getDims();
+        __values = new double [__dim];
+        for (int i = 0; i < __dim; i++)
+            __values[i] = copyThis.getValue(i);
+            
+    }
     
-    return *this;
-}
-
-const Point Point::operator*(double multBy) const // prevent (p1 * 2) = p2
-{
-    Point product(__dim);
-    product.__id = __id;
+    Point & Point::operator=(const Point &equate)
+    {
+        __id = equate.getId();
+        __dim = equate.getDims();
+        __values = new double [__dim];
+        for (int i = 0; i < __dim; i++)
+            __values[i] = equate.getValue(i);
+        return *this;
+    }
     
-    for (int i = 0; i < __dim; i++)
-        product.__values[i] = __values[i] * multBy;
+    Point::~Point()
+    {
+        delete __values;
+    }
     
-    return product;
-}
-
-const Point Point::operator/(double divideBy) const // p3 = p2 / 2
-{
-    Point quotient(__dim);
-    quotient.__id = __id;
     
-    for (int i = 0; i < __dim; i++)
-        quotient.__values[i] = __values[i] / divideBy;
     
-    return quotient;
-}
-
-double & Point::operator[](int index)
-{
-    return __values[index];
-}
-
-
-// Friends
-
-Point &operator+=(Point & addTo, const Point & add)
-{
-    for (int i = 0; i < addTo.getDims(); i++)
-        addTo.setValue(i, addTo.getValue(i) - add.getValue(i));
-    return addTo;
-}
-
-Point &operator-=(Point & subFrom, const Point & sub)
-{
-    for (int i = 0; i < subFrom.getDims(); i++)
-        subFrom.setValue(i, subFrom.getValue(i) - sub.getValue(i));
-    return subFrom;
-}
-
-const Point operator+(const Point & add1, const Point & add2)
-{
-    Point sum(add1.getDims());
-    for (int i = 0; i < sum.getDims(); i++)
-        sum[i] = add1.getValue(i) + add2.getValue(i);
-    return sum;
-}
-
-const Point operator-(const Point & sub1, const Point & sub2)
-{
-    Point diff(sub1.getDims());
-    for (int i = 0; i < diff.getDims(); i++)
-        diff[i] = sub1.getValue(i) - sub2.getValue(i);
-    return diff;
-}
-
-bool operator==(const Point & one, const Point & two)
-{
-    bool answer = true;
+    // Accessors & mutators
     
-    if (one.getDims() == two.getDims())
+    int Point::getId() const
+    {
+        return __id;
+    }
+    
+    int Point::getDims() const
+    {
+        return __dim;
+    }
+    
+    void Point::setValue(int i, double setTo)
+    {
+        __values[i] = setTo;
+    }
+    
+    double Point::getValue(int i) const
+    {
+        return __values[i];
+    }
+    
+    
+    
+    // Member functions
+    
+    double Point::distanceTo(const Point &other) const
+    {
+        if (__dim == other.getDims())
+        {
+            if (*this == other)
+                return 0;
+            else
+            {
+                double sum;
+            
+                for (int i = 0; i < __dim; i++)
+                    sum += pow((__values[i]-other.getValue(i)), 2);
+            
+                return sqrt(sum);
+            }
+            
+        }
+        else
+        {
+            std::cout << "Can't calculate between points in differnt dimensions" << std::endl;
+            return -1;
+        }
+    }
+    
+    
+    
+    // Other overloaded operators
+    
+    
+    // Members
+    
+    Point & Point::operator*=(double multBy) // p *= 6; p.operator*=(6)
+    {
+        for (int i = 0; i < __dim; i++)
+            __values[i] *= multBy;
+        
+        return *this;
+    }
+    
+    Point & Point::operator/=(double divideBy)
+    {
+        for (int i = 0; i < __dim; i++)
+            __values[i] /= divideBy;
+        
+        return *this;
+    }
+    
+    const Point Point::operator*(double multBy) const // prevent (p1 * 2) = p2
+    {
+        Point product(__dim);
+        product.__id = __id;
+        
+        for (int i = 0; i < __dim; i++)
+            product.__values[i] = __values[i] * multBy;
+        
+        return product;
+    }
+    
+    const Point Point::operator/(double divideBy) const // p3 = p2 / 2
+    {
+        Point quotient(__dim);
+        quotient.__id = __id;
+        
+        for (int i = 0; i < __dim; i++)
+            quotient.__values[i] = __values[i] / divideBy;
+        
+        return quotient;
+    }
+    
+    double & Point::operator[](int index)
+    {
+        return __values[index];
+    }
+    
+    
+    // Friends
+    
+    Point &operator+=(Point & addTo, const Point & add)
+    {
+        for (int i = 0; i < addTo.getDims(); i++)
+            addTo.setValue(i, addTo.getValue(i) + add.getValue(i));
+        return addTo;
+    }
+    
+    Point &operator-=(Point & subFrom, const Point & sub)
+    {
+        for (int i = 0; i < subFrom.getDims(); i++)
+            subFrom.setValue(i, subFrom.getValue(i) - sub.getValue(i));
+        return subFrom;
+    }
+    
+    const Point operator+(const Point & add1, const Point & add2)
+    {
+        Point sum(add1.getDims());
+        for (int i = 0; i < sum.getDims(); i++)
+            sum[i] = add1.getValue(i) + add2.getValue(i);
+        return sum;
+    }
+    
+    const Point operator-(const Point & sub1, const Point & sub2)
+    {
+        Point diff(sub1.getDims());
+        for (int i = 0; i < diff.getDims(); i++)
+            diff[i] = sub1.getValue(i) - sub2.getValue(i);
+        return diff;
+    }
+    
+    bool operator==(const Point & one, const Point & two)
+    {
+        bool answer = true;
+        
+        if (one.getDims() == two.getDims())
+        {
+            for (int i = 0; i < one.getDims(); i++)
+                if (one.getValue(i) != two.getValue(i))
+                    answer = false;
+        }
+        else
+            answer = false;
+        
+        return answer;
+    }
+    
+    bool operator!=(const Point & one, const Point & two)
+    {
+        bool answer = false;
+        
+        if (one.getDims() == two.getDims())
+        {
+            for (int i = 0; i < one.getDims(); i++)
+                if (one.getValue(i) == two.getValue(i))
+                    answer = true;
+        }
+        else
+            answer = true;
+        
+        return answer;
+    }
+    
+    bool operator<(const Point & one, const Point & two)
     {
         for (int i = 0; i < one.getDims(); i++)
+        {
+            if (one.getValue(i) < two.getValue(i))
+                return true;
             if (one.getValue(i) != two.getValue(i))
-                answer = false;
+                return false;
+        }
+        return false;
     }
-    else
-        answer = false;
     
-    return answer;
-}
-
-bool operator!=(const Point & one, const Point & two)
-{
-    bool answer = false;
-    
-    if (one.getDims() == two.getDims())
+    bool operator>(const Point & one, const Point & two)
     {
         for (int i = 0; i < one.getDims(); i++)
-            if (one.getValue(i) == two.getValue(i))
-                answer = true;
+        {
+            if (two.getValue(i) < one.getValue(i))
+                return true;
+            else if (two.getValue(i) != one.getValue(i))
+                return false;
+        }
+        return false;
     }
-    else
-        answer = true;
     
-    return answer;
-}
-
-bool operator<(const Point & one, const Point & two)
-{
-    for (int i = 0; i < one.getDims(); i++)
+    bool operator<=(const Point & one, const Point & two)
     {
-        if (one.getValue(i) < two.getValue(i))
+        if (one < two || one == two)
             return true;
-        if (one.getValue(i) != two.getValue(i))
-            return false;
+        return false;
     }
-    return false;
-}
-
-bool operator>(const Point & one, const Point & two)
-{
-    for (int i = 0; i < one.getDims(); i++)
+    
+    bool operator>=(const Point & one, const Point & two)
     {
-        if (two.getValue(i) < one.getValue(i))
+        if (one > two || one == two)
             return true;
-        if (two.getValue(i) != one.getValue(i))
-            return false;
+        return false;
     }
-    return false;
+    
+    std::ostream & operator<<(std::ostream & stream, const Point & p)
+    {
+        for (int i = 0; i < p.getDims(); i++)
+        {
+            stream << p.getValue(i);
+            
+            if (i != p.getDims()-1)
+                stream << ", ";
+        }
+            
+        return stream;
+    }
+    
+    std::istream & Clustering::operator>>(std::istream & stream, Point & p)
+    {
+        std::string buffer;
+        std::string::size_type size;
+        for (int i = 0; i < p.getDims(); i++)
+        {
+            stream >> buffer;
+            p.__values[i] = std::stod(buffer, &size);
+        }
+            
+        
+        return stream;
+    }
 }
-
-bool operator<=(const Point & one, const Point & two)
-{
-    return false;
-}
-
-bool operator>=(const Point & one, const Point & two)
-{
-    return false;
-}
-
-std::ostream & operator<<(std::ostream & stream, const Point & p)
-{
-    for (int i = 0; 9 < p.getDims(); i++)
-        stream << p.getValue(i);
-    return stream;
-}
-
-std::istream & Clustering::operator>>(std::istream & stream, Point & p)
-{
-    for (int i = 0; 9 < p.getDims(); i++)
-        stream >> p.__values[i];
-    return stream;
-}
-
